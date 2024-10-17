e4473a92dad848091bbc19ad35902972023ec236
lynchdiva <attamaill@gmail.com>
2024-10-18 00:00:23 +0400

    feat: finish task

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,44 +1,161 @@
-const toDoInput = document.getElementById('new-task');
-const toDoButton = document.querySelector('.to-do__button');
-const toDoList = document.querySelector('.to-do__list');
+'use strict';
 
-toDoButton.addEventListener('click', addTask);
-toDoList.addEventListener('click', markAsComplited);
+restoreTasksInList();
 
-function addTask() {
+function restoreTasksInList() {
+  const tasksData = JSON.parse(localStorage.getItem('tasksData'));
+
+  if (!tasksData) return;
+
+  const tasks = tasksData.map(taskData => {
+    const task = createTask(taskData.textContent, taskData.id);
+    const taskDoneCheckbox = task.querySelector('.to-do__done-input');
+
+    taskDoneCheckbox.checked = taskData.checked;
+
+    return task;
+  });
+
+  const toDoList = document.querySelector('.to-do__list');
+
+  tasks.forEach(task => toDoList.append(task));
+
+  showList();
+}
+
+const addButton = document.querySelector('.to-do__button');
+const clearButton = document.querySelector('#clear-list');
+
+addButton.addEventListener('click', addTaskToList);
+clearButton.addEventListener('click', clearList);
+
+function addTaskToList() {
+  const toDoInput = document.getElementById('new-task');
   const taskText = toDoInput.value;
 
   if (taskText === '') {
     return;
   }
 
-  const taskItem = document.createElement('li');
-  taskItem.classList.add('to-do__task');
-  taskItem.innerText = taskText;
+  if (isEmptyList()) {
+    showList();
+  }
 
-  addBin(taskItem);
+  const toDoList = document.querySelector('.to-do__list');
+  const task = createTask(taskText, null);
 
-  toDoList.append(taskItem);
+  toDoList.append(task);
 
   toDoInput.value = '';
+
+  addToLocalStorage(task);
 }
 
-function addBin(task) {
-  const bin = document.createElement('img');
-
-  bin.classList.add('to-do__bin');
-  bin.setAttribute('src', './assets/icons/bin.svg');
-  bin.setAttribute('alt', 'корзина');
-
-  bin.addEventListener('click', function () {
-    this.closest('li').remove();
-  });
-
-  task.append(bin);
+function isEmptyList() {
+  const noTasksPlaceholder = document.querySelector('.to-do__no-tasks');
+  return !noTasksPlaceholder.classList.contains('hidden');
 }
 
-function markAsComplited(evt) {
-  if (evt.target.tagName === 'LI') {
-    evt.target.classList.toggle('to-do__task_complited');
+function showList() {
+  const toDoList = document.querySelector('.to-do__list');
+
+  makeVisible(toDoList);
+
+  const noTasksPlaceholder = document.querySelector('.to-do__no-tasks');
+
+  makeInvisible(noTasksPlaceholder);
+
+  changeClearButtonState('abled');
+}
+
+function makeVisible(elem) {
+  elem.classList.remove('hidden');
+}
+
+function makeInvisible(elem) {
+  elem.classList.add('hidden');
+}
+
+function changeClearButtonState(state) {
+  const clearButton = document.querySelector('#clear-list');
+
+  if (state === 'disabled') {
+    clearButton.disabled = true;
+  } else if (state === 'abled') {
+    clearButton.disabled = false;
   }
+}
+
+function createTask(taskText, id) {
+  const taskItem = document.createElement('li');
+  taskItem.classList.add('to-do__task');
+  taskItem.textContent = taskText;
+
+  addTaskStatusIndicator(taskItem, id);
+
+  return taskItem;
+}
+
+function addTaskStatusIndicator(taskItem, id) {
+  taskItem.insertAdjacentHTML(
+    'beforeend',
+    `<label class="to-do__done-label"
+                ><input type="checkbox" class="to-do__done-input" /><span
+                  class="to-do__done-custom-input"
+                ></span
+              ></label>`
+  );
+  const taskDoneCheckbox = taskItem.querySelector('.to-do__done-input');
+
+  taskDoneCheckbox.id = id ? id : crypto.randomUUID();
+
+  addHandlerFor(taskDoneCheckbox);
+}
+
+function addHandlerFor(taskDoneCheckbox) {
+  taskDoneCheckbox.addEventListener('change', function () {
+    const tasksData = JSON.parse(localStorage.getItem('tasksData'));
+
+    tasksData.forEach(taskData => {
+      if (taskData.id === taskDoneCheckbox.id) {
+        taskData.checked = taskDoneCheckbox.checked;
+      }
+    });
+
+    localStorage.setItem('tasksData', JSON.stringify(tasksData));
+  });
+}
+
+function clearList() {
+  const toDoList = document.querySelector('.to-do__list');
+  toDoList.innerHTML = '';
+
+  makeInvisible(toDoList);
+
+  const noTasksPlaceholder = document.querySelector('.to-do__no-tasks');
+
+  makeVisible(noTasksPlaceholder);
+
+  changeClearButtonState('disabled');
+
+  localStorage.removeItem('tasksData');
+}
+
+function addToLocalStorage(task) {
+  if (!localStorage.getItem('tasksData')) {
+    localStorage.setItem('tasksData', JSON.stringify([]));
+  }
+
+  const taskDoneCheckbox = task.querySelector('.to-do__done-input');
+  const taskData = {
+    id: taskDoneCheckbox.id,
+    textContent: task.textContent,
+    checked: taskDoneCheckbox.checked
+  };
+
+  const tasksData = JSON.parse(localStorage.getItem('tasksData'));
+
+  tasksData.push(taskData);
+
+  localStorage.setItem('tasksData', JSON.stringify(tasksData));
 }
